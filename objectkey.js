6673fbc170f96b28eae0e76eed8ad80c1febafeb
vivek201088 <vivekgrdjaiswal@gmail.com>
2024-01-17 22:01:03 +0530

    Object key value to array

---
+++ objectkey.js
@@ -0,0 +1,11 @@
+const user={
+    username:"vivek",
+    password:"admin123",
+    age:35,
+    sex:'M'
+}
+
+const keys=Object.keys(user);
+const value=Object.values(user)
+console.log(keys);
+console.log(value);
