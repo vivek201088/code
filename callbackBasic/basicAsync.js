e3396a0af8c1bc5d37280722727158a2f773af2a
vivek201088 <vivekgrdjaiswal@gmail.com>
2024-01-31 20:50:07 +0530

    "basicAsync"

---
+++ callbackBasic/basicAsync.js
@@ -0,0 +1,8 @@
+console.log("Start");
+setTimeout(() => {
+    console.log("timeout");
+},3000);
+console.log("Doing this task");
+console.log("Doing this task");
+console.log("Doing this task");
+console.log("end");
