6c5bde06def4402c2eb32bd5cbcd15141ff76cd3
vivek201088 <vivekgrdjaiswal@gmail.com>
2024-01-31 20:15:41 +0530

    "add callback async"

--- callbackBasic/callbasic1.js
+++ callbackBasic/callbasic1.js
@@ -1,5 +1,4 @@
 //basic callbck
-
 function greet(name,callback){
     console.log(name);
     callback();
@@ -9,5 +8,19 @@ function welcome(){
     console.log("Welcome!!!");
 }
 
-
 greet("Naveen",welcome);
+
+//callback with async function
+function printInfo(name,callback){
+    //Async function/task
+     setTimeout(function(){
+      console.log("printing info for",name );
+      callback("plz call me back!!!");
+     },3000);
+}
+
+function displayMessage(message){
+    console.log(message);
+}
+
+printInfo("Lisa",displayMessage)
